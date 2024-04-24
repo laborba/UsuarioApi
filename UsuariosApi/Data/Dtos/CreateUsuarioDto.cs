1c7490e80980fc0d4bce6a2e99ff35973c16b4d0
laborba <lucasadeborba@gmail.com>
2024-04-24 14:55:39 -0300

    criado primeiros parametros, migrado ao banco e testado ok

---
+++ UsuariosApi/Data/Dtos/CreateUsuarioDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosApi.Data.Dtos;
+
+public class CreateUsuarioDto
+{
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public DateTime DataNascimento { get; set; }
+    [Required]
+    [DataType(DataType.Password)]    //Para explicitar que este campo é de senha (este comando não permite senhas simples (exemplo: "senha")
+    public string Password { get; set; }
+    [Required]
+    [Compare("Password")]            //Para confirmar se a senha foi digitada de forma igual
+    public string RePassword { get; set; }
+}
---
+++ UsuariosApi/Migrations/20240424175000_Corrigindo nomes de propriedades.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UsuariosApi.Migrations
+{
+    public partial class Corrigindonomesdepropriedades : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DanaNascimento",
+                table: "AspNetUsers",
+                newName: "DataNascimento");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DataNascimento",
+                table: "AspNetUsers",
+                newName: "DanaNascimento");
+        }
+    }
+}
